15adb74422a1377cf928a8741e358961db1834a1
layemee11 <struchkov.0234@gmail.com>
2023-06-12 15:42:27 +0900

    fix: product listing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,88 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Product from "./Product";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
+function Header() {
+  const headerStyle = {
+    backgroundColor: "#2b2625",
+    padding: "20px",
+    textAlign: "left",
+  };
+
+  return (
+    <header style={headerStyle}>
+      <h1>layemee</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  const footerStyle = {
+    backgroundColor: "#f2f2f2",
+    padding: "10px",
+    textAlign: "center",
+  };
+
+  return (
+    <footer style={footerStyle}>
+      <p>Все права защищены &copy; 2023</p>
+    </footer>
+  );
+}
+
+function Home() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    await fetch("https://dummyjson.com/products")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        console.log(resp);
+        setProducts(resp.products);
+      });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const productContainerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  };
+
+  const productStyle = {
+    width: "200px",
+    margin: "10px",
+  };
+
+  const productTitleStyle = {
+    fontWeight: "bold",
+  };
+
+  const productImageStyle = {
+    width: "100%",
+    height: "auto",
+  };
+
   return (
     <div>
       <Header />
-      <h1>Добро пожаловать на главную страницу!</h1>
-      <Product />
+      <div style={productContainerStyle}>
+        {products.map((item) => (
+          <div key={item.id} style={productStyle}>
+            <p style={productTitleStyle}>Товар: {item.title}</p>
+            <p>Цена: {item.price}</p>
+            <img
+              src={item.thumbnail}
+              alt={item.title}
+              style={productImageStyle}
+            />
+          </div>
+        ))}
+      </div>
       <Footer />
     </div>
   );
-};
+}
 
 export default Home;
